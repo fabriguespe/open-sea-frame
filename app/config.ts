cc3e4a15779c18bf63a1e679dad6322927e3f188
fabri <fguespe@gmail.com>
2024-04-02 16:19:43 -0400

    update

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,4 @@
 export const NEXT_PUBLIC_URL =
   process.env.NODE_ENV == 'development'
     ? 'https://162b-50-174-20-202.ngrok-free.app'
-    : 'https://open-sea-frames.vercel.app';
+    : 'https://open-sea-frame.vercel.app';
